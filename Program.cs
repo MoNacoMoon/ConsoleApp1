721026f41ee8148935a53ca6d312943efc8eead4
evol_ <evol_@HP>
2024-02-06 18:43:03 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.Write("A");
+            //}
+
+
+            //for (int i = 50; i >= 40; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //for (int t = 6; t <= 36; t+=6)
+            //{
+            //    Console.Write(t+ "  ");
+            //}
+
+
+            //int count = 0;
+            //int sum = 0;
+            //for (int t = 11; t <= 99; t += 11)
+            //{
+            //    Console.Write(t + " ");
+            //    count++;
+            //    sum += t;
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine($"Количество: {count}");
+            //Console.WriteLine($"Сумма: {sum}");
+
+
+            //int count = 0;
+            //for (int t = 100; t <= 999; t++)
+            //{
+            //    int number = t;
+            //    int sum = 0;
+
+            //    while (number > 0)
+            //    {
+            //        sum += number % 10;
+            //        number /= 10;
+            //    }
+
+            //    if (sum == 20)
+            //    {
+            //        Console.Write( t + " ");
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine($"Количество: {count}");
+
+
+            //Console.Write("Введите сумму вклада: ");
+            //double depositAmount = double.Parse(Console.ReadLine());
+
+            //Console.Write("Введите количество месяцев: ");
+            //int months = int.Parse(Console.ReadLine());
+            //for (int t = 0; t < months; t++)
+            //{
+            //    depositAmount += depositAmount * 0.07;
+            //}
+            //Console.WriteLine($"Конечная сумма вклада после {months} месяцев: {depositAmount}");
+
+
+            //double initialPrice = 100;
+            //double currentPrice = initialPrice;
+            //double profit;
+
+            //Console.WriteLine("Стоимость акций:");
+
+            //for (int month = 1; month <= 10; month++)
+            //{
+            //    Console.WriteLine($"{month} месяц = {currentPrice:F2} руб.");
+            //    currentPrice += currentPrice * 0.10;
+            //}
+            //currentPrice = initialPrice;
+            //for (int month = 1; month <= 7; month++)
+            //{
+            //    currentPrice += currentPrice * 0.10;
+            //}
+            //double priceOf1000Shares = currentPrice * 1000;
+
+            //Console.WriteLine($"Стоимость 1000 акций на 7 месяц = {priceOf1000Shares:F2} руб.");
+            //Console.ReadLine();
+        }
+    }
+}
